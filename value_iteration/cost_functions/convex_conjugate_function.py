bug when Compute average loss

value_iteration/cost_functions/convex_conjugate_function.py
@@ -215,29 +215,18 @@ class ArcTangent(ConvexConjugateFunction):
     def __call__(self, x):
         if isinstance(x, np.ndarray):
             assert np.all(np.abs(x) <= self.a)
-            shape = x.shape
-
-            x = x.reshape((-1, self.n, 1))
-            g = -2.0 * self.b * self.a / np.pi * np.log(np.clip(np.cos(np.pi / (2. * self.a) * x), 0.0, 1.0)).reshape(shape)
-
+            g = -2.0 * self.b * self.a / np.pi * np.log(np.clip(np.cos(np.pi / (2. * self.a) * x), 0.0, 1.0))
             if self.n > 1:
                 g = np.sum(g, axis=1,keepdims=True)
-            print(g.shape,'g')
 
         elif isinstance(x, torch.Tensor):
             x = torch.clamp(x, -self.a, self.a)
             assert torch.all(torch.abs(x) <= self.a)
-
-            shape = x.shape
-            print(shape)
-            x = x.view(-1, self.n, 1)
-            g = -2.0 * self.b * self.a / np.pi * torch.log(torch.clamp(torch.cos(np.pi / (2. * self.a) * x), 0.0, 1.0)).view(shape)
-            print(g.shape,'g')
+            g = -2.0 * self.b * self.a / np.pi * torch.log(torch.clamp(torch.cos(np.pi / (2. * self.a) * x), 0.0, 1.0))
             if self.n > 1:
                 g = torch.sum(g, axis=1,keepdim=True)
         else:
             raise ValueError("x must be either an numpy.ndarray or torch.Tensor, but is type {0}.".format(type(x)))
-        print(g.shape,'g')
         return g
 
     def grad(self, x):
@@ -247,8 +236,6 @@ class ArcTangent(ConvexConjugateFunction):
             shape = x.shape
             x = x.reshape((-1, self.n, 1))
             g_grad = self.b * np.tan(np.pi/(2.*self.a) * x).reshape(shape)
-            # if self.n > 1:
-            #     g_grad = np.sum(g_grad, axis=1,keepdims=True)
 
 
         elif isinstance(x, torch.Tensor):
@@ -257,8 +244,6 @@ class ArcTangent(ConvexConjugateFunction):
             shape = x.shape
             x = x.view(-1, self.n, 1)
             g_grad = self.b * torch.tan(np.pi/(2.*self.a) * x).view(shape)
-            # if self.n > 1:
-            #     g_grad = torch.sum(g_grad, axis=1,keepdim=True)
 
         else:
             raise ValueError("x must be either an numpy.ndarray or torch.Tensor, but is type {0}.".format(type(x)))
@@ -291,21 +276,15 @@ class ArcTangent(ConvexConjugateFunction):
 
     def grad_convex_conjugate(self, x):
         if isinstance(x, np.ndarray):
-
-            shape = x.shape
-            x = x.reshape((-1, self.n, 1))
-            g_star_grad = 2. * self.a / np.pi * np.arctan(x / self.b).reshape(shape)
+            g_star_grad = 2. * self.a / np.pi * np.arctan(x / self.b)
 
         elif isinstance(x, torch.Tensor):
-
-            shape = x.shape
-            x = x.view(-1, self.n, 1)
-            g_star_grad = 2. * self.a / np.pi * torch.atan(x / self.b).view(shape)
+            g_star_grad = 2. * self.a / np.pi * torch.atan(x / self.b)
 
         else:
             raise ValueError("x must be either an numpy.ndarray or torch.Tensor, but is type {0}.".format(type(x)))
-
-        return g_star_grad
+        
+        return g_star_grad.to(torch.float32)
 
     def hessian_convex_conjugate(self, x):
         if isinstance(x, np.ndarray):
@@ -335,8 +314,7 @@ def convex_conjugate_test(fun, verbose=True, plot=True):
     y_torch = torch.from_numpy(y).float()
 
     g = fun(x)
-    print(g.shape,'g')
-    print(x.shape,'x')
+    
     g_star = fun.convex_conjugate(y)
     g_torch = fun(x_torch).numpy()
     g_star_torch = fun.convex_conjugate(y_torch).numpy()

value_iteration/doulbe_pendulum.py
@@ -333,7 +333,7 @@ class DoulbePendulumLogCos(DoulbePendulum):
 
         # Create the dynamics:
         super(DoulbePendulumLogCos, self).__init__(cuda=cuda, **kwargs)
-        self.u_lim = torch.tensor([6., 0])
+        self.u_lim = torch.tensor([[6.], [1]])
         device = torch.device("cuda" if cuda else "cpu")
 
         # Create the Reward Function:
@@ -343,14 +343,16 @@ class DoulbePendulumLogCos(DoulbePendulum):
         assert R.size == self.n_act and np.all(R > 0.0)
         self.R = np.diag(R).reshape((self.n_act, self.n_act))
 
+
         self._q = SineQuadraticCost(self.Q, np.array([0.0, 1.0, 0.0, 0.0]), cuda=cuda)
         self.q = BarrierCost(self._q,  self.x_penalty, cuda)
-
+        # print('self.R:',self.u_lim.view(-1,1)*self.R)
         # Determine beta s.t. the curvature at u = 0 is identical to 2R
         beta = 4. * self.u_lim ** 2 / np.pi * self.R
+        beta = torch.diag(beta).view(self.n_act, 1)
         # beta = torch.tensor([40,40]) 
         self.u_lim = self.u_lim.to(device)
-        self.r = ArcTangent(alpha=self.u_lim, beta=beta.numpy()[0, 0])
+        self.r = ArcTangent(alpha=self.u_lim, beta=beta)
 
     def rwd(self, x, u):
         return self.q(x) + self.r(u)

value_iteration/test.py
@@ -1,45 +1,20 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import torch
-# 定义奖励函数
-def action_cost(u, umax, beta):
-    return -2 * beta * umax / np.pi * np.log(np.cos(np.pi * u / (2 * umax)))
+x = torch.ones(103,2, 1)
+# b = torch.tensor([1,1])
+b = torch.ones(2,1)
+d =x/b
+print('x/b:',d.shape)
+# 创建一个二维对角线矩阵
+# matrix = torch.tensor([[1, 0, 0],
+#                        [0, 2, 0],
+#                        [0, 0, 3]])
 
+# # 提取对角线元素
+# diag_elements = torch.diag(matrix)
+# print(diag_elements)
+# # 将其转换为形状 (2, 1)
+# # 注意，提取的对角线元素数量应与目标形状匹配
+# # 这里对角线元素的数量是 3，因此我们将其转换为 (3, 1)
+# result = diag_elements.view(-1, 1)
 
-g = np.zeros([100,2,1])
-g = np.sum(g, axis=1)
-alpha = np.array([1.0, 0.5])
-print((-np.abs(alpha), +np.abs(alpha)))
-
-# 参数设置
-# umax = 1.0  # 动作的最大值
-# beta = 1.0  # 正的常数
-# R = np.array([1.0, 0.5])
-# R = np.diag(R).reshape((2, 2))
-
-# u_lim =  torch.tensor([6., 0])
-
-# beta = 4. * u_lim[0] ** 2 / np.pi * R
-
-# print(beta)
-# alpha=u_lim.numpy()
-# print(alpha.shape[0],'o')
-# beta=beta.numpy()[0, 0]
-# print(beta)
-# 生成动作范围
-# u = np.linspace(-umax, umax, 500)
-
-# # 计算动作成本
-# cost = action_cost(u, umax, beta)
-# print(action_cost(1.0, umax, beta))
-
-
-# # 绘制图像
-# plt.figure(figsize=(10, 6))
-# plt.plot(u, cost)
-# plt.xlabel('Action (u)')
-# plt.ylabel('Cost')
-# plt.title('Action Cost Function')
-# plt.legend()
-# plt.grid(True)
-# plt.show()
+# print(result)

value_iteration/update_value_function.py
@@ -92,10 +92,6 @@ def update_value_function(step_i, value_fun_tar, system, mem_train, hyper, write
 
                 # Compute the reward:
                 r_j = -hyper['dt'] * (system.q(xj) + system.r(uj_star))
-                # print('uj_star',uj_star.shape) 
-                # print('system.q(xj) ',system.q(xj).shape)  # torch.Size([128, 1, 1])
-                # print('system.r',system.r(uj_star).shape)  #torch.Size([128, 2, 1])
-                # print('r_j', r_j.shape)  #torch.Size([128, 2, 1])
                 r = r + hyper["gamma"] ** n * r_j
 
                 # Compute adversarial state-noise:
@@ -107,12 +103,6 @@ def update_value_function(step_i, value_fun_tar, system, mem_train, hyper, write
                 xi_u = float(hyper["robust"]) * norm(z_u) * xi_u_scale[n]
 
                 # Compute adversarial observation-noise:
-                # print(dBjdx.shape, uj_star.shape)  #torch.Size([25600, 4, 4, 2]) torch.Size([25600, 2, 1])
-                # print(uj_star.unsqueeze(-1).shape) #torch.Size([25600, 2, 1, 1])
-                # print(torch.matmul(dBjdx, uj_star.unsqueeze(-1)).shape)
-                # print(torch.matmul(dBjdx, uj_star.unsqueeze(-1)).squeeze(-1).shape)
-                # print(dajdx.shape, dVjdx.shape)
-
                 # torch.matmul(dBjdx, uj_star.unsqueeze(-1)).squeeze(-1)
                 z_o = -torch.matmul((uj_star.permute(0, 2, 1).unsqueeze(1) * dBjdx).sum(dim=-1)+ dajdx, dVjdx)
                 # z_o = -torch.matmul(torch.matmul(dBjdx, uj_star.unsqueeze(-1)).squeeze(-1) + dajdx, dVjdx)
@@ -141,19 +131,12 @@ def update_value_function(step_i, value_fun_tar, system, mem_train, hyper, write
 
                 # Compute the value function of the next state:
                 Vn, dVndx, un_star, dundx_star = policy(xn, Bn, system.r, value_fun_tar)
-                print('r', r.shape)  #torch.Size([128, 2, 1])
-                print('Vn',Vn.shape)  #torch.Size([128, 1, 1])
-                print('gamma', hyper["gamma"])
-                print('v0_tar shape',torch.clamp(r + hyper['gamma'] ** (n+1) * Vn, max=0.0).shape)  #torch.Size([128, 2, 1])
 
                 # Compute the target value function:
                 V0_tar.append(torch.clamp(r + hyper['gamma'] ** (n+1) * Vn, max=0.0))
 
                 xj, Vj, dVjdx, uj_star, dujdx_star, dajdx, Bj, dBjdx = xn, Vn, dVndx, un_star, dundx_star, dandx, Bn, dBndx
 
-            print('w_lambda',w_lambda.shape) #torch.Size([1, 4, 1])
-            print('len(V0_tar)',len(V0_tar))  #4
-            print('V0_tar',torch.cat(V0_tar, dim=1).shape)   #[128, 2, 1])
             # Compute Exponential Average of the n-steps:
             Vn = torch.sum(w_lambda * torch.cat(V0_tar, dim=1), dim=1, keepdim=True)
 
@@ -223,6 +206,7 @@ def update_value_function(step_i, value_fun_tar, system, mem_train, hyper, write
 
         # Compute average loss statistics
         if verbose and (epoch_i == 0 or np.mod(epoch_i+1, 5) == 0):
+            #TODO something wrong here
             loss, loss_V = torch.stack(loss), torch.stack(loss_V)
             stats_loss, stats_loss_V = analyze_error(loss), analyze_error(loss_V)
             print_loss(epoch_i, stats_loss, stats_loss_V, time.perf_counter() - t0_start)